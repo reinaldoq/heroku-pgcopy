37d2e5894154e18b61e0672a1426fcbcfc8e0d24
Reinaldo Quintero <reinaldoqm@gmail.com>
2024-01-19 13:44:45 +0100

    Initial commit

--- heroku_pgcopy.sh
+++ heroku_pgcopy.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
 # Set your app name
-APP_NAME="product-pipe-reinaldo-q-0q2ezt"
+APP_NAME=""
 
 # Set the plan for the new database
 NEW_DATABASE_PLAN="mini"
 
 # Set the name for the new and old databases
-OLD_DATABASE_NAME="postgresql-curly-58731"
+OLD_DATABASE_NAME=""
 
 # Step 1: Provision a new database
-heroku addons:create heroku-postgresql:$NEW_DATABASE_PLAN --app $APP_NAME
+
+NEW_DATABASE_NAME="$(heroku addons:create heroku-postgresql:$NEW_DATABASE_PLAN --app $APP_NAME | grep -E 'Created postgresql-[a-z]+-[0-9]+ as HEROKU_POSTGRESQL_[A-Z]+_URL' | awk '{print $2'})"
 
 # Wait for the new database to be ready
 heroku pg:wait -a $APP_NAME
 
 # Step 2: Enter maintenance mode
-heroku maintenance:on -a $APP_NAME
+heroku maintenance:on -a $APP_NAME --confirm
 
 # Step 3: Transfer data to the new database
 heroku pg:copy DATABASE_URL $NEW_DATABASE_NAME --app $APP_NAME
